90078a8c68bb59405fc3bd09898934c2fc1be666
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-15 11:53:58 +0530

    added some views

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def home():
+    return { 'message':'home' }
+
+@app.get("/about/")
+def about():
+    return { 'message':'about' }
+
+@app.get("/contact")
+def contact():
+    return { 'message':'contact' }
