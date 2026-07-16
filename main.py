58cacc4ecdbcd8cf58e6cf1584c218569bbc0628
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-16 14:28:40 +0530

    made a calcultor

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/")
-def home():
-    return { 'message':'home' }
 
-@app.get("/about")
-def about():
-    return { 'message':'about' }
-
-@app.get("/contact")
-def contact():
-    return { 'message':'contact' }
+@app.get("/calculate/{a}/{symbol}/{b}")
+def calculator(a:int, b:int, symbol:str):
+    if symbol=="add":
+        return {'answer':a+b}
+    elif symbol=="subtract":
+        return {'answer':a-b}
+    elif symbol=="multiply":
+        return {'answer':a*b}
+    elif symbol=="divide":
+        return {'message':a/b}
+    else:
+        return {'message':'enter +, -, *, /'}
